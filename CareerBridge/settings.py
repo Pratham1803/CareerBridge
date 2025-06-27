d9ab87b14cb5d745c596b4e274bb98e0ebfd66ba
pratham1803 <114798779+Pratham1803@users.noreply.github.com>
2025-06-27 10:23:56 +0530

    Reintroduce create_superuser management command with environment variable support

--- CareerBridge/settings.py
+++ CareerBridge/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9b2_!v6%4_&-gks5^9u_f3sf=uar6nfr*^nx@)ptjgtlxys9lh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['careerbridge-m3ir.onrender.com', 'localhost', '127.0.0.1']
 
